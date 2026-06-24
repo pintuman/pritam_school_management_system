b2e3809899f9aa9e3643729c71cd0a0f6470a4c9
pintuman <pmanandhar@gmail.com>
2026-06-24 20:22:15 +0545

    Hardcode allowed hosts

--- school_management/settings.py
+++ school_management/settings.py
@@ -16,7 +16,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '412290272116283268')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = [
+    'pritamschoolmanagementsystem-production.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
